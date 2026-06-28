40b7ca799c4868dd19362895e1afa0f4eb0eee94
Rishik Reddy <nallarishik58@gmail.com>
2026-06-28 23:14:34 +0530

    app is running and errors are removed

--- src/index.js
+++ src/index.js
@@ -2,16 +2,17 @@ import express from "express"
 import {main} from "./db/index.js" 
 import dotenv from "dotenv"
 import  Mongoose  from "mongoose"
+import {app} from "./app.js"
 dotenv.config()
-const app=express()
 
+// console.log("INDEX.JS IS RUNNING");
 main()
 .then(
     ()=>{
         console.log("DATABASE IS CONNECTED ")
-    app.listen((process.env.process,()=>{
+    app.listen(process.env.PORT,()=>{
         console.log(`Server is listerning on the ${process.env.PORT}`);
-    }))
+    })
 })
 .catch((err)=>{
     console.log("DATABASE FAILED TO CONNECT",err);
